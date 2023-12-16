152b821ee91a9ddd132e86170f94b6ccd342e8df
Justin Walker <justinwwalker@pm.me>
2023-12-15 19:04:23 -0600

    new GENERun class

--- genelib.py
+++ genelib.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from pathlib import Path
+import parIOWrapper as parw
 import re
 
 
@@ -11,6 +13,23 @@ def check_suffix(run_number):
         match = re.search(r"([0-9]{1,4})$", run_number)
         suffix = "_" + match.group(0).zfill(4)
     else:
-        print("Please enter a valid run number, e.g. .dat or 0123")
-        return None
+        ValueError("Please enter a valid run number, e.g. .dat or 0123")
     return suffix
+
+
+class GENERun(object):
+    """Documentation for GENERun object"""
+
+    def __init__(self, parameters_filepath: str):
+        self.init_paths(parameters_filepath)
+
+    def init_paths(self, parameters_filepath):
+        filepath = Path(parameters_filepath)
+        self.dir_path = str(filepath.parent) + "/"
+        self.par_path = str(filepath)
+        self.suffix = check_suffix(parameters_filepath)
+        self.runnumber = self.suffix.strip("_").strip(".")
+        # print(self.runnumber)
+
+    def read_parameters(self):
+        self.pars = parw.create_parameters_dict(self.par_path)
